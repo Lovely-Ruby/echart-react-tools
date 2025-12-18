30d9d3052c851ed45851a40f4a6722615442a320
wangzhongqing <wangzhongqing0303@qq.com>
2025-12-18 14:54:21 +0800

    chore: save

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,17 @@ import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    <>
-      <LineChart />
-    </>
+    <div style={{ width: 600, height: 300 }}>
+      <LineChart
+        xAxis={['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']}
+        data={[
+          {
+            label: '订单数',
+            data: [120, 200, 150, 80, 70, 110, 130],
+          },
+        ]}
+      />
+    </div>
   )
 }
 
